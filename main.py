e0a32976e34331066e91536e0cb7259a31e5b838
Ares16x16 <ed700a@gmail.com>
2024-04-13 18:46:53 +0800

    added modes

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from tkinter import colorchooser
 import pyaudio
 import threading
 
+import mode
+
 # Constants
-CHUNK_SIZE = 1024  # Number of audio frames per buffer
+CHUNK_SIZE = 2048  # Number of audio frames per buffer
 FORMAT = pyaudio.paInt16  # Audio format (16-bit)
 CHANNELS = 1  # Mono audio
 RATE = 44100  # Sample rate (Hz)
@@ -13,6 +15,7 @@ RATE = 44100  # Sample rate (Hz)
 # Initialize Tkinter
 window = tk.Tk()
 window.title("Real-time Audio Visualizer")
+window.resizable(False, False)
 
 # Set up the Tkinter canvas
 WINDOW_WIDTH = 800
@@ -51,53 +54,143 @@ class AudioVisualizer:
 
         # Initialize settings
         self.bar_color = "#ffffff"  # Default color: white
+        self.visualization_mode = {
+            "Average Horizontal Rectangle": mode.average_horizontal_rectangle,  # Default
+            "Anti-Aliasing Filter Horizontal Rectangle": mode.anti_aliasing_filter_horizontal_rectangle,
+            "Fast Fourier Transform Horizontal Rectangle": mode.fft_horizontal_rectangle,
+            "Anti-Aliasing Filter Vertical Circle": mode.anti_aliasing_filter_vertical_circle,
+            "Anti-Aliasing Filter Vertical Inner Circle": mode.anti_aliasing_filter_vertical_inner_circle,
+            "Anti-Aliasing Filter Circle": mode.anti_aliasing_filter_circle,
+        }
+        self.current_mode_name = "Average Horizontal Rectangle"
 
         # Create menu bar
         menubar = tk.Menu(window)
         window.config(menu=menubar)
 
-        # Create color submenu
+        """
+        Create color submenu
+        """
         color_menu = tk.Menu(menubar, tearoff=0)
         menubar.add_cascade(label="Color", menu=color_menu)
-
         # Add circle palette to color submenu
         color_menu.add_command(label="Set Color", command=self.open_color_palette)
 
-        # Create a threading Event for synchronization
+        """
+        Create input and output devices submenu
+        """
+        input_devices_menu = tk.Menu(menubar, tearoff=0)
+        menubar.add_cascade(label="Input Device", menu=input_devices_menu)
+        output_devices_menu = tk.Menu(menubar, tearoff=0)
+        menubar.add_cascade(label="Output Device", menu=output_devices_menu)
+
+        # Populate devices submenu
+        input_devices, output_devices = self.get_devices()
+        for i, device in enumerate(input_devices):
+            input_devices_menu.add_command(
+                label=device["name"],
+                command=lambda index=device["index"]: self.set_input_device(index),
+            )
+        for i, device in enumerate(output_devices):
+            output_devices_menu.add_command(
+                label=device["name"],
+                command=lambda index=device["index"]: self.set_output_device(index),
+            )
+
+        """
+        Create mode submenu
+        """
+        mode_menu = tk.Menu(menubar, tearoff=0)
+        menubar.add_cascade(label="Visualization Mode", menu=mode_menu)
+        mode_menu.add_command(
+            label="Average - Horizontal Rectangle",
+            command=lambda: self.set_mode("Average Horizontal Rectangle"),
+        )
+        mode_menu.add_command(
+            label="Anti-Aliasing Filter - Horizontal Rectangle",
+            command=lambda: self.set_mode("Anti-Aliasing Filter Horizontal Rectangle"),
+        )
+        mode_menu.add_command(
+            label="Anti-Aliasing Filter - Vertical Circle",
+            command=lambda: self.set_mode("Anti-Aliasing Filter Vertical Circle"),
+        )
+        mode_menu.add_command(
+            label="Anti-Aliasing Filter - Vertical Inner Circle",
+            command=lambda: self.set_mode("Anti-Aliasing Filter Vertical Inner Circle"),
+        )
+        mode_menu.add_command(
+            label="Anti-Aliasing Filter - Circle",
+            command=lambda: self.set_mode("Anti-Aliasing Filter Circle"),
+        )
+        mode_menu.add_command(
+            label="Fast Fourier Transform - Horizontal Rectangle",
+            command=lambda: self.set_mode(
+                "Fast Fourier Transform Horizontal Rectangle"
+            ),
+        )
+
         self.audio_event = threading.Event()
+
+    def get_devices(self):
+        input_devices, output_devices = [], []
+        info = self.p.get_host_api_info_by_index(0)
+        device_count = info.get("deviceCount")
+        for i in range(device_count):
+            device_info = self.p.get_device_info_by_index(i)
+            if device_info["maxInputChannels"] > 0:
+                input_devices.append({"index": i, "name": device_info["name"]})
+            if device_info["maxOutputChannels"] > 0:
+                output_devices.append({"index": i, "name": device_info["name"]})
+        return input_devices, output_devices
+
+    def set_input_device(self, index):
+        self.input_device_index = index
+        self.input_stream = self.p.open(
+            format=FORMAT,
+            channels=CHANNELS,
+            rate=RATE,
+            input=True,
+            input_device_index=self.input_device_index,
+            frames_per_buffer=CHUNK_SIZE,
+        )
+        window.update()
+
+    def set_output_device(self, index):
+        self.output_device_index = index
+        self.output_stream = self.p.open(
+            format=FORMAT,
+            channels=CHANNELS,
+            rate=RATE,
+            output=True,
+            output_device_index=self.output_device_index,
+            frames_per_buffer=CHUNK_SIZE,
+        )
+        window.update()
 
     def open_color_palette(self):
         color = colorchooser.askcolor(title="Choose Color")
         self.bar_color = color[1] if color[1] else self.bar_color
+
+    def set_mode(self, selected_mode):
+        self.current_mode_name = selected_mode
 
     def process_audio(self):
         while True:
             audio_buffer = self.input_stream.read(CHUNK_SIZE)
             audio_data = np.frombuffer(audio_buffer, dtype=np.int16)
 
-            # Notify the drawing function
-            window.after(0, self.draw_bars, audio_data)
-
+            # drawing function
+            window.after(0, self.visualize, audio_data)
             self.output_stream.write(audio_buffer)
 
-    def draw_bars(self, audio_data):
+    def visualize(self, audio_data):
         canvas.delete("all")
-        bar_width = 5
-        bar_heights = np.abs(audio_data) // 100
-
-        for i in range(CHUNK_SIZE):
-            bar_x = i * bar_width
-            bar_y = WINDOW_HEIGHT / 2
-            canvas.create_rectangle(
-                bar_x,
-                bar_y,
-                bar_x + bar_width,
-                bar_y - bar_heights[i],
-                fill=self.bar_color,
-            )
+        # print(self.current_mode_name)
+        self.visualization_mode[self.current_mode_name](
+            visualizer, window, canvas, audio_data
+        )
 
     def start_visualization(self):
-        # Start the audio processing thread
         self.process_audio_thread = threading.Thread(target=self.process_audio)
         self.process_audio_thread.daemon = True
         self.process_audio_thread.start()
@@ -105,7 +198,6 @@ class AudioVisualizer:
     def run(self):
         self.start_visualization()
 
-        # Start the Tkinter main loop
         window.mainloop()
 
         # Cleanup
@@ -116,6 +208,6 @@ class AudioVisualizer:
         self.p.terminate()
 
 
-# Usage
-visualizer = AudioVisualizer(input_device_index=7, output_device_index=12)
-visualizer.run()
+if __name__ == "__main__":
+    visualizer = AudioVisualizer(input_device_index=7, output_device_index=12)
+    visualizer.run()
---
+++ mode.py
@@ -0,0 +1,278 @@
+import numpy as np
+import cmath
+import tkinter as tk
+import pyaudio
+from scipy import signal
+
+# from scipy.fft import fft
+
+"""
+Average Horizontal Rectangle
+"""
+
+
+def downsample_mean(audio_data, bar_width, window_width):
+    new_length = window_width // bar_width - 5
+    segment_length = len(audio_data) // new_length
+    downsampled_audio = np.zeros(new_length)
+
+    for i in range(new_length):
+        start_index = i * segment_length
+        end_index = start_index + segment_length
+        segment = audio_data[start_index:end_index]
+        downsampled_audio[i] = np.mean(segment)
+
+    return downsampled_audio
+
+
+def average_horizontal_rectangle(visualizer, window, canvas, audio_data):
+    bar_width = 3
+    window_width = window.winfo_width()
+    window_height = window.winfo_height()
+
+    downsample_audio = downsample_mean(audio_data, bar_width, window_width)
+
+    bar_heights = np.abs(downsample_audio) // 75
+    audio_length = len(downsample_audio)
+
+    for i in range(audio_length):
+        bar_x = i * bar_width + (window_width / 2) - (audio_length * bar_width / 2)
+        bar_y = window_height / 2
+        canvas.create_rectangle(
+            bar_x,
+            bar_y,
+            bar_x + bar_width,
+            bar_y - bar_heights[i],
+            fill=visualizer.bar_color,
+        )
+
+
+"""
+Anti-Aliasing Filter Horizontal Rectangle
+"""
+
+
+def downsample_with_filter(audio_data, factor):
+    # downsampling by an integer factor
+    downsampled_audio = signal.decimate(audio_data, factor, zero_phase=True)
+
+    # anti-aliasing filter
+    cutoff_freq = 0.5 / factor
+    b, a = signal.butter(4, cutoff_freq, analog=False)
+    filtered_audio = signal.lfilter(b, a, downsampled_audio)
+
+    return filtered_audio
+
+
+def anti_aliasing_filter_horizontal_rectangle(visualizer, window, canvas, audio_data):
+    bar_width = 3
+    window_width = window.winfo_width()
+    window_height = window.winfo_height()
+
+    audio_length = window_width // bar_width - 5
+    segment_length = len(audio_data) // audio_length
+    downsampled_audio = downsample_with_filter(audio_data, segment_length)
+    bar_height = np.abs(downsampled_audio) // 75
+
+    for i in range(audio_length):
+        start_index = i * segment_length
+        end_index = start_index + segment_length
+        segment = downsampled_audio[start_index:end_index]
+
+        bar_x = i * bar_width + (window_width / 2) - (audio_length * bar_width / 2)
+        bar_y = window_height / 2
+        canvas.create_rectangle(
+            bar_x,
+            bar_y,
+            bar_x + bar_width,
+            bar_y - bar_height[i],
+            fill=visualizer.bar_color,
+        )
+
+
+"""
+Fast Fourier Transform Horizontal Rectangle
+"""
+
+
+def fft(data):
+    N = len(data)
+    if N <= 1:
+        return data
+    even = fft(data[0::2])
+    odd = fft(data[1::2])
+
+    twiddle_factors = [cmath.exp(-2j * cmath.pi * k / N) for k in range(N // 2)]
+    transformed_data = [0] * N
+
+    for k in range(N // 2):
+        transformed_data[k] = even[k] + twiddle_factors[k] * odd[k]
+        transformed_data[k + N // 2] = even[k] - twiddle_factors[k] * odd[k]
+
+    return transformed_data
+
+
+def fft_horizontal_rectangle(visualizer, window, canvas, audio_data):
+    bar_width = 3
+    window_width = window.winfo_width()
+    window_height = window.winfo_height()
+
+    audio_length = window_width // bar_width - 5
+    segment_length = len(audio_data) // audio_length
+    fft_data = fft(audio_data)
+
+    for i in range(audio_length):
+        start_index = i * segment_length
+        end_index = start_index + segment_length
+        segment = fft_data[start_index:end_index]
+        magnitudes = np.abs(segment)
+        bar_height = np.mean(magnitudes) // 1000
+
+        bar_x = i * bar_width + (window_width / 2) - (audio_length * bar_width / 2)
+        bar_y = window_height - 10
+        canvas.create_rectangle(
+            bar_x,
+            bar_y,
+            bar_x + bar_width,
+            bar_y - bar_height,
+            fill=visualizer.bar_color,
+        )
+
+
+"""
+Anti-Aliasing Filter Circle
+"""
+
+
+def anti_aliasing_filter_vertical_circle(visualizer, window, canvas, audio_data):
+    bar_width = 3
+    window_width = window.winfo_width()
+    window_height = window.winfo_height()
+
+    audio_length = window_width // bar_width - 5
+    segment_length = len(audio_data) // audio_length
+    downsampled_audio = downsample_with_filter(audio_data, segment_length)
+    bar_height = np.abs(downsampled_audio) // 75
+
+    circle_center_x = window_width / 2
+    circle_center_y = window_height / 2
+
+    circle_radius = min(circle_center_x, circle_center_y) - 40
+
+    for i in range(audio_length):
+        start_index = i * segment_length
+        end_index = start_index + segment_length
+        segment = downsampled_audio[start_index:end_index]
+
+        angle = 2 * np.pi * i / audio_length
+
+        bar_x = circle_center_x + circle_radius * np.cos(angle) - bar_width / 2
+        bar_y = circle_center_y + circle_radius * np.sin(angle) - bar_height[i] / 2
+
+        canvas.create_rectangle(
+            bar_x,
+            bar_y,
+            bar_x + bar_width,
+            bar_y + bar_height[i],
+            fill=visualizer.bar_color,
+        )
+
+
+def anti_aliasing_filter_vertical_inner_circle(visualizer, window, canvas, audio_data):
+    bar_width = 3
+    window_width = window.winfo_width()
+    window_height = window.winfo_height()
+
+    audio_length = window_width // bar_width - 5
+    segment_length = len(audio_data) // audio_length
+    downsampled_audio = downsample_with_filter(audio_data, segment_length)
+    bar_height = np.abs(downsampled_audio) // 75
+
+    circle_center_x = window_width / 2
+    circle_center_y = window_height / 2
+
+    circle_radius = min(circle_center_x, circle_center_y) - 40
+
+    for i in range(audio_length):
+        start_index = i * segment_length
+        end_index = start_index + segment_length
+        segment = downsampled_audio[start_index:end_index]
+
+        angle = 2 * np.pi * i / audio_length
+        bar_x = circle_center_x + circle_radius * np.cos(angle) - bar_width / 2
+        bar_y = circle_center_y + circle_radius * np.sin(angle) - bar_height[i] / 2
+        angle_to_center = np.arctan2(circle_center_y - bar_y, circle_center_x - bar_x)
+
+        # Align
+        adjusted_bar_x = bar_x + bar_width / 2 * np.cos(angle_to_center)
+        adjusted_bar_y = bar_y + bar_height[i] / 2 * np.sin(angle_to_center)
+
+        canvas.create_rectangle(
+            adjusted_bar_x,
+            adjusted_bar_y,
+            adjusted_bar_x + bar_width,
+            adjusted_bar_y + bar_height[i],
+            fill=visualizer.bar_color,
+        )
+
+
+""" Circle (but something went wrong)"""
+
+
+def rotate_point(x, y, angle, center_x, center_y):
+    x -= center_x
+    y -= center_y
+    new_x = x * np.cos(angle) - y * np.sin(angle)
+    new_y = x * np.sin(angle) + y * np.cos(angle)
+    return new_x + center_x, new_y + center_y
+
+
+def draw_rotated_rectangle(canvas, x, y, width, height, angle, fill_color):
+    rect_center_x = x + width / 2
+    rect_center_y = y + height / 2
+
+    points = [(x, y), (x + width, y), (x + width, y + height), (x, y + height)]
+
+    rotated_points = [
+        rotate_point(px, py, angle, rect_center_x, rect_center_y) for px, py in points
+    ]
+    flattened_rotated_points = [coord for point in rotated_points for coord in point]
+    canvas.create_polygon(flattened_rotated_points, fill=fill_color)
+
+
+def anti_aliasing_filter_circle(visualizer, window, canvas, audio_data):
+    bar_width = 3
+    window_width = window.winfo_width()
+    window_height = window.winfo_height()
+
+    audio_length = window_width // bar_width - 5
+    segment_length = len(audio_data) // audio_length
+    downsampled_audio = downsample_with_filter(audio_data, segment_length)
+    bar_height = np.abs(downsampled_audio) // 75
+
+    circle_center_x = window_width / 2
+    circle_center_y = window_height / 2
+
+    circle_radius = min(circle_center_x, circle_center_y) - 40
+
+    for i in range(audio_length):
+        start_index = i * segment_length
+        end_index = start_index + segment_length
+        segment = downsampled_audio[start_index:end_index]
+
+        angle = 2 * np.pi * i / audio_length
+
+        bar_x = circle_center_x + circle_radius * np.cos(angle) - bar_width / 2
+        bar_y = circle_center_y + circle_radius * np.sin(angle) - bar_height[i]
+
+        angle_to_center = np.arctan2(circle_center_y - bar_y, circle_center_x - bar_x)
+
+        draw_rotated_rectangle(
+            canvas,
+            bar_x,
+            bar_y,
+            bar_width,
+            bar_height[i],
+            angle_to_center,
+            visualizer.bar_color,
+        )
